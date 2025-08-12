1173b0ae9d1d9c8a14ead41e0d839609deba8624
cleyxcode <afiafikadewi3@gmail.com>
2025-08-13 00:54:39 +0900

    update di admin panels

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,8 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->globalSearch()
+            ->databaseNotifications()
             ->font('Inter') 
             ->brandName('PETUGAS SINARA')
             ->colors([
