bff4b3ec4224c8dd1e9d162e07b0bfcf42e27432
cleyxcode <afiafikadewi3@gmail.com>
2025-08-13 00:51:32 +0900

    update font di admin panels

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->font('Inter') 
             ->brandName('PETUGAS SINARA')
             ->colors([
                 'primary' => Color::Amber,
